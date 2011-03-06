579a2f2b4cb3d3bdb4fac79ddc80879dcbf28ec6
nyapoo <nyapoo@gmail.com>
2011-03-06 22:32:36 +0900

    first commit

---
+++ HomeBank/Form1.h
@@ -0,0 +1,67 @@
+#pragma once
+
+
+namespace HomeBank {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Form1 の概要
+	///
+	/// 警告: このクラスの名前を変更する場合、このクラスが依存するすべての .resx ファイルに関連付けられた
+	///          マネージ リソース コンパイラ ツールに対して 'Resource File Name' プロパティを
+	///          変更する必要があります。この変更を行わないと、
+	///          デザイナと、このフォームに関連付けられたローカライズ済みリソースとが、
+	///          正しく相互に利用できなくなります。
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ここにコンストラクタ コードを追加します
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// 必要なデザイナ変数です。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディタで変更しないでください。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"Form1";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
+
---
+++ HomeBank/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : 標準インクルード HomeBank.pch のみを
+// 含むソース ファイルは、プリコンパイル済みヘッダーになります。
+// stdafx.obj にはプリコンパイル済み型情報が含まれます。
+
+#include "stdafx.h"
+
+
